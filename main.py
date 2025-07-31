73776514877e404fcce86caa55d9bfa97102b977
beto9915 <alcs1491@gmail.com>
2025-07-30 18:45:30 -0600

    Version2

--- main.py
+++ main.py
@@ -17,7 +17,22 @@ class Program:
                         b=int(input("Ingrese segundo numero: "))
                         mcd=Program.mcd(a,b)
                         print(f"El mcd es: {mcd}")
+
                     case 2:
+                        palabra=input("Ingrese la palabra: ")
+                        veces=int(input("Ingrese cuantas veces: "))
+                        print(f"{Program.n_veces(palabra, veces)}")
+                    case 3:
+                        cadena=input("Ingrese cadena de texto: ")
+                        letra=input("Ingrese la letra: ")
+                        cantidad=Program.letra_en_cadena(cadena, letra)
+                        print(f"La cantidad de veces que aparece {letra} en {cadena} es: {cantidad}")
+                    case 4:
+
+
+            except:
+                print(f"Error")
+
     @staticmethod
     def mcd(a, b):
         if b == 0:
@@ -25,4 +40,17 @@ class Program:
         else:
             return Program.mcd(b, a % b)
     @staticmethod
-    def n_veces():
+    def n_veces(palabra, veces):
+        if veces==0:
+            return 0
+        else:
+            return palabra & Program.n_veces(palabra, veces)
+    @staticmethod
+    def letra_en_cadena(cadena, letra):
+        if cadena == "":
+            return 0
+        elif cadena[0] == letra:
+            return 1 + Program.letra_en_cadena(cadena[1:], letra)
+        else:
+            return Program.letra_en_cadena(cadena[1:], letra)
+    @staticmethod
